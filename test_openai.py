bb5686139ae60c9525f9039b82ad2ae99a019d96
Eric Bezzam <ebezzam@gmail.com>
2023-11-11 12:26:42 +0100

    EXtract text.

--- test_openai.py
+++ test_openai.py
@@ -16,25 +16,28 @@ client = OpenAI(
     organization="org-bcv27ooZj8JyXgpgj5sed8rH",
 )
 
-# # -- chat
-# response = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a helpful assistant."},
-#     {"role": "user", "content": "Who won the world series in 2020?"},
-#     {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-#     {"role": "user", "content": "Where was it played?"}
-#   ]
-# )
-# print(response)
-
-# -- image
-response = client.images.generate(
-  model="dall-e-3",
-  prompt="a white siamese cat",
-  size="1024x1024",
-  quality="standard",
-  n=1,
+# -- chat
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  # giving history of chat
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
 )
-image_url = response.data[0].url
-print(image_url)
+
+# extract text
+print(response.choices[0].message.content)
+
+# # -- image
+# response = client.images.generate(
+#   model="dall-e-3",
+#   prompt="a white siamese cat",
+#   size="1024x1024",
+#   quality="standard",
+#   n=1,
+# )
+# image_url = response.data[0].url
+# print(image_url)
