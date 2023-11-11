50ab2a1569b5e925d6ee22a4f3cd73e11e702b00
Eric Bezzam <ebezzam@gmail.com>
2023-11-11 12:18:46 +0100

    Update openai example.

--- test_openai.py
+++ test_openai.py
@@ -11,17 +11,30 @@ from openai import OpenAI
 from keys import OPENAI_KEY
 
 client = OpenAI(
-    api_key=OPENAI_KEY
+    api_key=OPENAI_KEY,
+    # LauzHack
+    organization="org-bcv27ooZj8JyXgpgj5sed8rH",
 )
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-  ]
-)
+# # -- chat
+# response = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": "Who won the world series in 2020?"},
+#     {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+#     {"role": "user", "content": "Where was it played?"}
+#   ]
+# )
+# print(response)
 
-print(response)
+# -- image
+response = client.images.generate(
+  model="dall-e-3",
+  prompt="a white siamese cat",
+  size="1024x1024",
+  quality="standard",
+  n=1,
+)
+image_url = response.data[0].url
+print(image_url)
