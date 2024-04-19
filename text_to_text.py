0db7f3e0717fae08dcb2740906d83367ed646461
Matthias Wyss <matthiaswyss74@gmail.com>
2024-04-19 22:37:52 +0200

    ajout du detect_language

--- text_to_text.py
+++ text_to_text.py
@@ -1,6 +1,7 @@
 import requests
 from keys import HUGGING_FACE_KEY
 import time
+from detect_language import detect_language
 
 
 API_URL = "https://api-inference.huggingface.co/models/facebook/mbart-large-50-many-to-many-mmt"
@@ -9,17 +10,19 @@ headers = {"Authorization": f"Bearer {HUGGING_FACE_KEY}"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
 
-start_time = time.time()
-#output = query({
-#	"inputs": "Меня зовут Вольфганг и я живу в Берлине",
-#	"parameters": {"src_lang": "ru_RU", "tgt_lang": "fr_FR"}
-#})
-output = query({
-	"inputs": "Ciao, mi chiamo Matthias e oggi facciamo un hackaton",
-	"parameters": {"src_lang": "it_IT", "tgt_lang": "fr_XX"}
-})
+def text_to_text(input: str):
+    input_language = detect_language(input)
+    #print(input_language)
+    output = query({
+        "inputs": input,
+        "parameters": {"src_lang": input_language, "tgt_lang": "fr_XX"}
+    })
+    return output[0]['translation_text']
 
-print(output)
-print("Time taken:", time.time() - start_time)
+
+# testing
+input1 = "Ciao, mi chiamo Matthias e oggi facciamo un hackaton"
+input2 = "Меня зовут Вольфганг и я живу в Берлине"
+print(text_to_text(input2))
+
